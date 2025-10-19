1785bcc728a59775f43fecb5b0ad292d3d9d7a58
Cory Ferguson <cferguson@live.ca>
2025-10-18 22:18:14 -0600

    fix __init__.py

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,5 +1,5 @@
 """ProxiMeter FastAPI application package.
 
-The FastAPI application is defined in wsgi.py.
+The FastAPI application is defined in main.py.
 This __init__.py is kept minimal to avoid circular imports.
 """
